7607db5c45bbc59a37db651f6b12b66595a73070
River Pearl <riverpearl0115@gmail.com>
2016-07-25 15:31:21 +0900

    FragmentTabHost

---
+++ app/src/main/java/com/tacademy/sampletab/FragmentTabActivity.java
@@ -0,0 +1,41 @@
+package com.tacademy.sampletab;
+
+import android.support.v4.app.FragmentTabHost;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.Toast;
+
+public class FragmentTabActivity extends AppCompatActivity {
+
+    //MainActivity처럼 했을 경우 event를 받아 값을 주고받을 때 문제가 생김.
+
+    FragmentTabHost tabHost;
+
+    public final static String TAB_ONE = "tab1";
+    public final static String TAB_TWO = "tab2";
+    public final static String TAB_THREE = "tab3";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_tab);
+
+        tabHost = (FragmentTabHost)findViewById(R.id.tabhost);
+        tabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec(TAB_ONE).setIndicator("TAB1"), OneFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec(TAB_TWO).setIndicator("TAB2"), TwoFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec(TAB_THREE).setIndicator("TAB3"), TwoFragment.class, null);
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String s) {
+                if (TAB_TWO.equals(s))
+                    Toast.makeText(FragmentTabActivity.this, "TAB_TWO", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        tabHost.setCurrentTabByTag(TAB_TWO);
+    }
+}
---
+++ app/src/main/java/com/tacademy/sampletab/OneFragment.java
@@ -0,0 +1,38 @@
+package com.tacademy.sampletab;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OneFragment extends Fragment {
+
+
+    public OneFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentTabHost tabHost;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+        tabHost = (FragmentTabHost)view.findViewById(R.id.tabhost);
+        tabHost.setup(getContext(), getChildFragmentManager(), android.R.id.tabcontent);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator("CT1"), OneOneFragment.class, null);
+        tabHost.addTab(tabHost.newTabSpec("tab2").setIndicator("CT2"), OneOneFragment.class, null);
+
+        return view;
+    }
+
+}
